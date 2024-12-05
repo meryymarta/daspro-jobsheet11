bff21c8758e9f43312cd6c4b3e0d952b77948eee
meryymarta <meriammartadinata@gmail.com>
2024-12-05 18:55:22 +0700

    experiment 2.4 question 3

--- ExpressingGratitude_12.java
+++ ExpressingGratitude_12.java
@@ -14,7 +14,17 @@ public class ExpressingGratitude_12 {
         System.out.println("Thank you " + name + " for being the best teacher in the world. \n" +
         "You inspired in me a love for learning and made me feel like I could ask you anything.");
     }
+
+    public static void sayAdditionalGreetings(String additionalMessage) {
+        System.out.println("Additional Greeting: " + additionalMessage);
+    }
+    
     public static void main(String[]args){
         sayThankyou();
+
+        String additionalMessage = "continue inspiring others!";
+        sayAdditionalGreetings(additionalMessage);
     }
 }
+    
+
